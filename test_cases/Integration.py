5e8c871210a80f0dfa104c95e1b136a4ac5a5b53
bahaeddine20 <bahaeddine.somrani@esprit.tn>
2025-04-25 01:02:27 +0100

    fix problem adb,appium server docker (linux)

--- test_cases/Integration.py
+++ test_cases/Integration.py
@@ -26,10 +26,11 @@ def setup_driver(device):
     options.device_name = device
     options.adb_exec_timeout = 60000
     #options.remote_adb_host="host.docker.internal"
-    #options.remote_adb_host="host.docker.internal"
-    remote_url = "http://127.0.0.1:4723"
+    options.remote_adb_host="host.docker.internal"
+    #remote_url = "http://127.0.0.1:4723"
     #remote_url = "http://172.21.0.3:4723"
-    #remote_url = "http://appium:4723"
+    remote_url = "http://appium:4723"
+    options.uiautomator2ServerPort = 8201
 
     options.set_capability("enforceXPath1", True)
     return webdriver.Remote(remote_url, options=options)
